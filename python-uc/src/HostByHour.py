9ab7ad38091cd869bad85d3b8fbe8793ae4f9ff3
Fabian Astudillo <fabian.astudillo.salinas@gmail.com>
2022-03-31 11:37:16 -0500

    Add HostByHour.py

---
+++ python-uc/src/HostByHour.py
@@ -0,0 +1,44 @@
+#import runpy
+#import datetime
+import datetime
+import dateutil.parser
+from FingerprintGenerator import FingerprintGenerator
+#import socks
+import socket
+import logging
+import os
+
+def main():
+
+    #socks.set_default_proxy(socks.SOCKS5, "localhost", 9000)
+    #socket.socket = socks.socksocket
+
+    #date_from=date.fromisoformat('2021-08-23T00:00:00.000000Z')
+    date_from=dateutil.parser.isoparse('2022-03-30T12:00:00Z')
+
+    date_upto=dateutil.parser.isoparse("2022-03-30T16:00:00Z")
+    date_offset = date_from
+    
+    IP_ES = os.getenv('IP_ES')
+    if not IP_ES:
+        IP="elasticsearch"
+
+    try:
+        fpg = FingerprintGenerator(IP_ES, date_from, "/root/whitelist.txt")
+    except Exception as ex:
+        logging.info(str(ex))
+
+    #fpg.GetIndices()
+    
+    while (date_offset <= date_upto):
+        try:
+            fpg.SetDatestep(date_offset)
+            fpg.getHostByHour()
+            #runpy.run_path(path_name='/fingerprint_generator.py -d ' + date_from.strftime("%Y-%m-%dT%H:%M:%SZ") + '-o ' + 0 + '-w "/root/whitelist.txt" -i "elasticsearch"')
+        except Exception as ex:
+            logging.info(str(ex))
+        date_offset+=datetime.timedelta(hours=1)
+    
+
+if __name__ == "__main__":
+    main()
